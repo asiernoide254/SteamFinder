5d2e5d1b0c11a460e4817e3d75b276d908f1b3c5
Carlos-PB <0619967480@uma.es>
2022-06-02 00:02:37 +0200

    Arreglado bugs con el formato de las imágenes.

--- js/search.js
+++ js/search.js
@@ -1,17 +1,38 @@
 function search() {
     // Declare variables
-    var input, filter, cards, a, i, txtValue;
-    input = document.getElementById("searchBar");
-    filter = input.value.toUpperCase();
-    container = document.getElementsByClassName("contenedor-videojuegos")[0];
-    cards = document.getElementsByClassName("card");
+    var input = document.getElementById("searchBar");
+    var filter = input.value.toUpperCase();
+    var container = document.getElementsByClassName("contenedor-videojuegos")[0];
+    var searchText = document.getElementById("searchText");
+    var cards = document.getElementsByClassName("card");
+    var cardImages = document.getElementsByClassName("card-img");
 
-    container.style.gridTemplateColumns = (filter == "" ? "1fr 1fr" : "100%");
+    if (filter == "") {
+        container.style.gridTemplateColumns = "";
+        searchText.innerHTML = "Juegos más populares";
+    } else {
+        container.style.gridTemplateColumns = "100%";
+        searchText.innerHTML = "Resultados";
+    }
+
+    /*for (var i = 0; i < cardImages.length; i++) {
+        if (filter == "") {
+            cardImages[i].style.width = "40%";
+        } else {
+            cardImages[i].style.width = "25%";
+        }
+    }*/
   
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < cards.length; i++) {
-      a = cards[i].getElementsByTagName("h5")[0];
+    for (var i = 0; i < cards.length; i++) {
+      var card = cards[i];
+      a = card.getElementsByTagName("h5")[0];
       txtValue = a.textContent || a.innerText;
-      cards[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
+
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        card.style.display = "";
+      } else {
+        card.style.display = "none";
+      }
     }
   }
