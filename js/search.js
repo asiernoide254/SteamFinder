3ad5259ea8bff8a82a4569717a25e1e1dabac6a9
Carlos-PB <0619967480@uma.es>
2022-06-01 22:31:20 +0200

    Modificado grid-template-columns al realizar una búsqueda de un videojuego.

--- js/search.js
+++ js/search.js
@@ -1,18 +1,17 @@
 function search() {
     // Declare variables
     var input, filter, cards, a, i, txtValue;
-    input = document.getElementById('searchBar');
+    input = document.getElementById("searchBar");
     filter = input.value.toUpperCase();
-    cards = document.getElementsByClassName('card');
+    container = document.getElementsByClassName("contenedor-videojuegos")[0];
+    cards = document.getElementsByClassName("card");
+
+    container.style.gridTemplateColumns = (filter == "" ? "1fr 1fr" : "100%");
   
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < cards.length; i++) {
       a = cards[i].getElementsByTagName("h5")[0];
       txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        cards[i].style.display = "";
-      } else {
-        cards[i].style.display = "none";
-      }
+      cards[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
     }
   }
