a684fd862d31adece31eb6dda2316c7c627136f7
Carlos-PB <0619967480@uma.es>
2022-06-05 21:20:16 +0200

    Añadido mensaje de error en la búsqueda de usuarios.

--- js/search.js
+++ js/search.js
@@ -1,48 +1,63 @@
 function searchVg() {
-    // Declare variables
-    var input = document.getElementById("searchBar");
-    var filter = input.value.toUpperCase();
-    var container = document.getElementsByClassName("contenedor-videojuegos")[0];
-    var searchText = document.getElementById("searchText");
-    var cards = document.getElementsByClassName("card");
-    var cardImages = document.getElementsByClassName("card-img");
-    var resultsFound = false;
+  var input = document.getElementById("vgSearchBar");
+  var filter = input.value.toUpperCase();
+  var container = document.getElementsByClassName("contenedor-videojuegos")[0];
+  var searchText = document.getElementById("searchText");
+  var cards = document.getElementsByClassName("card");
+  var cardImages = document.getElementsByClassName("card-img");
+  var resultsFound = false;
 
-    if (filter == "") {
-        container.style.gridTemplateColumns = "";
-        searchText.innerHTML = "Juegos más populares";
-    } else {
-        container.style.gridTemplateColumns = "100%";
-        searchText.innerHTML = "Resultados";
-    }
+  if (filter == "") {
+      container.style.gridTemplateColumns = "";
+      searchText.innerHTML = "Juegos más populares";
+  } else {
+      container.style.gridTemplateColumns = "100%";
+      searchText.innerHTML = "Resultados";
+  }
 
-    //Se jode en vista de movil
-    for (var i = 0; i < cardImages.length; i++) {
-        if (filter == "") {
-            cardImages[i].style.width = "40%";
-        } else {
-            cardImages[i].style.width = "25%";
-        }
-    }
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (var i = 0; i < cards.length; i++) {
-      var card = cards[i];
-      a = card.getElementsByTagName("h5")[0];
-      txtValue = a.textContent || a.innerText;
-
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        card.style.display = "";
-        resultsFound = true;
+  //Se jode en vista de movil
+  for (var i = 0; i < cardImages.length; i++) {
+      if (filter == "") {
+          cardImages[i].style.width = "40%";
       } else {
-        card.style.display = "none";
+          cardImages[i].style.width = "25%";
       }
+  }
+
+  for (var i = 0; i < cards.length; i++) {
+    var card = cards[i];
+    a = card.getElementsByTagName("h5")[0];
+    txtValue = a.textContent || a.innerText;
+
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      card.style.display = "";
+      resultsFound = true;
+    } else {
+      card.style.display = "none";
     }
-
-    var vgSearchError = document.getElementById("vgSearchError");
-    vgSearchError.hidden = resultsFound;
   }
 
-  function searchUser() {
-    
-  }
+  var vgSearchError = document.getElementById("vgSearchError");
+  vgSearchError.hidden = resultsFound;
+}
+
+function addEnterListener() {
+  var input = document.getElementById("userSearchBar");
+  input.addEventListener("keypress", function(event) {
+    if(event.key == "Enter") {
+      searchUser();
+    }
+  });
+}
+  
+function searchUser() {
+  var input = document.getElementById("userSearchBar");
+  var filter = input.value.toUpperCase();
+
+    if (filter == "DARKVOID") {
+      window.location.href = "usuario.html";
+    } else {
+      var userSearchError = document.getElementById("userSearchError");
+      userSearchError.style.visibility = "";
+    }
+}
